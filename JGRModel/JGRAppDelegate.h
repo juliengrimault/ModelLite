0b45b43e9036e7432427985f2c636c06a68cee69
Julien Grimault <grimaultjulien@gmail.com>
2014-03-09 13:12:43 +0800

    Initial Commit

---
+++ JGRModel/JGRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JGRAppDelegate.h
+//  JGRModel
+//
+//  Created by Julien on 9/3/14.
+//  Copyright (c) 2014 juliengrimault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
